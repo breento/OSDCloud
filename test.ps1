781db400baa2116b21c8080ae25a474c5aa9254c
breento <thomas@computerplace.nl>
2021-08-12 15:40:02 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,12 +1,17 @@
 Write-Host "Hello, Welcome to OSDCLoud"
 
-Write-Host "Set Resolution"
-Set-DisRes 1600
+#Write-Host "Set Resolution"
+#Set-DisRes 1600
 
-Update-Module OSD -Force
-Import-Module OSD -Force
+#Update-Module OSD -Force
+#Import-Module OSD -Force
 
 
 
-Write-Host "Starting OSD Cloud"
-Start-OSDCloud
+#Write-Host "Starting OSD Cloud"
+#Start-OSDCloud
+
+$Uri = 'https://dev.azure.com/breento-devops/test/_apis/git/repositories/test/items?path=OSDCloud/Start-BreentoOSDCloud.ps1&download=true&api-version=6.0'
+$script = Invoke-RestMethod -Uri $url -Method Get 
+Invoke-Expression $script
+
