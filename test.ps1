708d9e63870feb0a0829a4cc3198dba256bcf03d
breento <thomas@computerplace.nl>
2021-08-19 13:35:17 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -11,7 +11,7 @@ Write-Host "Hello, Welcome to OSDCLoud"
 #Write-Host "Starting OSD Cloud"
 #Start-OSDCloud
 
-$Uri = 'https://dev.azure.com/breento-devops/test/_apis/git/repositories/test/items?path=OSDCloud/Start-BreentoOSDCloud.ps1&download=true&api-version=6.0'
+$Uri = 'https://dev.azure.com/breento-devops/test/_apis/git/repositories/test/items?path=OSDCloud/02-Start-BreentoOSDCloud.ps1&download=true&api-version=6.0'
 $script = Invoke-RestMethod -Uri $uri -Method Get 
 Invoke-Expression $script
 
